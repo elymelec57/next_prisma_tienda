31206060f41eb42adefb275e4de5503d473993a1
ely colmenarez <elymelecc084@gmail.com>
2026-01-02 17:20:34 -0400

    realizando ajustes para que funcione el deploy en vercel

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,7 +1,7 @@
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-// export const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
-const { PrismaClient } = require('../../generated/prisma')
+// const { PrismaClient } = require('../../generated/prisma')
 
-export const prisma = new PrismaClient()
+// export const prisma = new PrismaClient()
