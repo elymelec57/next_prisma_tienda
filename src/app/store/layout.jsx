4a272a09203cb94d7a4599938bde14d76bcacef1
ely colmenarez <elymelecc084@gmail.com>
2026-01-02 15:53:26 -0400

    cambiando algo sencillo

--- src/app/store/layout.jsx
+++ src/app/store/layout.jsx
@@ -1,4 +1,23 @@
-import LayoutDashboard from "../dashboard/layout";
+import NavBar from "@/components/NavBar";
 import 'react-toastify/dist/ReactToastify.css';
 
-export default LayoutDashboard
+import { cookies } from 'next/headers'
+import jwt from "jsonwebtoken";
+import { ToastContainer } from 'react-toastify';
+
+export default async function LayoutDashboard({ children }) {
+
+    const cookieStore = await cookies()
+    const token = cookieStore.get('token')
+    const decoded = jwt.verify(token.value, process.env.JWT_TOKEN);
+
+    return (
+        <>
+            <div>
+                <NavBar data={decoded.data} />
+            </div>
+            {children}
+            <ToastContainer />
+        </>
+    );
+}
